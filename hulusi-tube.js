79c61f20a72c3068558f467bd25fb9542f2fbc5b
Paul M. Christian <paulmchristian@gmail.com>
2020-05-21 15:10:38 -0500

    some organization; thicker tongue gap

--- hulusi-tube.js
+++ hulusi-tube.js
@@ -6,12 +6,12 @@
 // file       : logo.jscad
 
 const tongueCut = ({
-                     borderW = 0.4,
-                     tongueBaseW = 2,
-                     tonguePointW = 1,
-                     tongueShaftL = 15,
-                     tongueTipL = 4,
-                   } = {}) => rectangular_extrude([
+  borderW = 0.4,
+  tongueBaseW = 2,
+  tonguePointW = 1,
+  tongueShaftL = 15,
+  tongueTipL = 4,
+} = {}) => rectangular_extrude([
   [tongueBaseW / -2, 0],
   [tonguePointW / -2, tongueShaftL],
   [0, tongueShaftL + tongueTipL],
@@ -23,13 +23,13 @@ const tongueCut = ({
   closed: false
 });
 
-const base = ({
-                bottomBorder = 1,
-                length = 30,
-                thickness = 0.3,
-                tongueCutProps,
-                width = 4,
-              } = {}) =>
+const reedAssembly = ({
+  bottomBorder = 1,
+  length = 30,
+  thickness = 0.3,
+  tongueCutProps,
+  width = 4,
+} = {}) =>
   cube({
     center: [true, false, false],
     size: [width, length, thickness],
@@ -39,73 +39,46 @@ const base = ({
         .translate([0, bottomBorder, 0])
     );
 
-// const main = () => [1]
-//       .map((x, i, {  } = {}) =>
-//
-//            union([
-//
-//              // reed assembly
-//              base({
-//                bottomBorder: 3,
-//                length: 30,
-//                thickness: 0.5,
-//                width: 7,
-//
-//                tongueCutProps: {
-//                  borderW: 0.1,
-//                  tongueBaseW: 4,
-//                  tonguePointW: 2,
-//                  tongueShaftL: 20,
-//                  tongueTipL: 0,
-//                }
-//              })
-//                .translate([i * 9, 0, 0]),
-//
-//
-//              // bottom wall with cutout for reed assembly
-//              cube({
-//                center: [true, false, false],
-//                size: [11, 100, 1],
-//              })
-//                .subtract(
-//                  cube({
-//                    center: [true, false, true],
-//                    size: [7, 30, 2],
-//                  })
-//                ),
-//
-//
-//
-//            ])
-//
-//           )
 
 const main = () => {
 
+  /*
+  * 150mm -> ~290Hz
+  * 225mm -> ~220Hz
+  * 187.5 --> ~
+  * */
+
   const {
-    tubeLength = 150,
+    tubeLength = 120, //187.5, //225,//150,
     wallThickness = 1,
-    r = 6,
+    tubeDiameter = 12,
+    tubeSides = 3,
   } = {};
 
   // const fn = 7;
   // const rot = 360 / 28;
   // const heightAdjust = r * 0.901;
 
-  const fn = 3;
-  const rot = 360 / 12;
+  const r = tubeDiameter / 2;
+  const rot = 360 / (tubeSides * 4);
   const heightAdjust = r * 0.5;
 
-  const cyl = cylinder({r, h: tubeLength, fn: 3})
+  const cyl = cylinder({r, h: tubeLength, fn: tubeSides})
     .rotateX(90)
     .rotateY(rot);
 
   const bore = cylinder({
-    r: r - wallThickness, h: tubeLength, fn: 3
+    r: r - wallThickness,
+    h: tubeLength,
+    fn: tubeSides,
   })
     .rotateX(90)
     .rotateY(rot)
     .translate([0, wallThickness, 0]);
+
+  const reedAssyLength = 30;
+  const reedAssyWidth = 7;
+  const reedThickness = 0.5;
 
   return union([
     cyl
@@ -114,19 +87,19 @@ const main = () => {
       .subtract(
         cube({
           center: [true, false, true],
-          size: [7, 30, 2],
+          size: [reedAssyWidth, reedAssyLength, 2],
         })
       ),
 
-    // reed assembly
-    base({
+    reedAssembly({
       bottomBorder: 3,
-      length: 30,
-      thickness: 0.5,
-      width: 7,
+      length: reedAssyLength,
+      thickness: reedThickness,
+      width: reedAssyWidth,
 
+      //TODO: try adding reinforcement to make more rigid
       tongueCutProps: {
-        borderW: 0.2,
+        borderW: 0.25,
         tongueBaseW: 4,
         tonguePointW: 2,
         tongueShaftL: 20,
