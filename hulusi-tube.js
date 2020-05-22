0440f349c43f42cd5e65878513a11edf6bc793b4
Paul M. Christian <paulmchristian@gmail.com>
2020-05-21 21:58:05 -0500

    finding the smallest stable tube/reed. In what ways and how much does reed placement matter?

--- hulusi-tube.js
+++ hulusi-tube.js
@@ -39,22 +39,17 @@ const reedAssembly = ({
         .translate([0, bottomBorder, 0])
     );
 
-
-const main = () => {
-
-  /*
-  * 150mm -> ~290Hz
-  * 225mm -> ~220Hz
-  * 187.5 --> ~
-  * */
-
-  const {
-    tubeLength = 120, //187.5, //225,//150,
-    wallThickness = 1,
-    tubeDiameter = 12,
-    tubeSides = 3,
-  } = {};
-
+const hulusiTube = ({
+  reedOffset = 0,
+  reedPointWidth = 0.25,
+  reedLength = 15,
+  reedThickness = 0.3,
+  reedWidth = 5,
+  tubeDiameter = 11,
+  tubeLength = 120, //187.5, //225,//150,
+  tubeSides = 3,
+  wallThickness = 1,
+}) => {
   // const fn = 7;
   // const rot = 360 / 28;
   // const heightAdjust = r * 0.901;
@@ -77,36 +72,68 @@ const main = () => {
     .translate([0, wallThickness, 0]);
 
   const reedAssyLength = 30;
-  const reedAssyWidth = 7;
-  const reedThickness = 0.5;
+  const reedAssyWidth = reedWidth + 1;
 
   return union([
     cyl
       .subtract(bore)
-      .translate([0, tubeLength - 10, heightAdjust])
+      .translate([0, tubeLength - wallThickness, heightAdjust])
       .subtract(
         cube({
           center: [true, false, true],
-          size: [reedAssyWidth, reedAssyLength, 2],
+          size: [reedAssyWidth, reedAssyLength, wallThickness],
         })
       ),
 
     reedAssembly({
-      bottomBorder: 3,
+      bottomBorder: reedOffset,
       length: reedAssyLength,
       thickness: reedThickness,
       width: reedAssyWidth,
 
       //TODO: try adding reinforcement to make more rigid
       tongueCutProps: {
-        borderW: 0.25,
-        tongueBaseW: 4,
-        tonguePointW: 2,
-        tongueShaftL: 20,
+        borderW: 0.23,
+        tongueBaseW: reedWidth,
+        tonguePointW: reedPointWidth,
+        tongueShaftL: reedLength,
         tongueTipL: 0,
       }
     })
       .translate([0, 0, 0]),
   ]);
+};
+
+
+const main = () => {
+
+  /*
+  * 150mm -> ~290Hz
+  * 225mm -> ~220Hz
+  * 187.5 --> ~
+  * */
+
+
+  /* Looking for optimal reed placement.
+   How much does distance to edge impact pitch?
+  *  */
+
+  return [
+    ...[1, /*2,*/ 3, /*4,*/ 5].map(i => hulusiTube({
+      reedPointWidth: 2,//0.25 * i,
+      reedOffset: i,
+      reedWidth: 6,
+      tubeLength: 150,
+    }).translate([6 * i, 0, 0])),
+    ...[/*1, 2, 3,*/ 4, /*5*/].map(i => hulusiTube({
+      reedPointWidth: 2,//0.25 * i,
+      reedOffset: i,
+      reedWidth: 6,
+      tubeLength: 75,
+    }).translate([ 12 * i, 0, 0]))
+  ];
+
+
+
 }
 
